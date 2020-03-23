c7bec2fcec7276c5ca4e603d179e1e345d99cab8
Sejin Kim <kim3@kenyon.edu>
2020-03-23 11:18:48 -0400

    Update 3-1.py

--- Chapter3/Ex3-1.py
+++ Chapter3/Ex3-1.py
@@ -1,1 +1,23 @@
+from sklearn.datasets import fetch_openml
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
+# Import the MNIST dataset
+mnist = fetch_openml('mnist_784', version=1)
+mnist.keys()
+
+# Look at the arrays
+X, y = mnist["data"], mnist["target"]
+X.shape
+y.shape
+
+# Try to plot the picture
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap="binary")
+plt.axis("off")
+plt.show()
+
+# What is that supposed to be?
+y[0]
