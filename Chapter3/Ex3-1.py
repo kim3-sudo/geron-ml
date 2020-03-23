292fdf5bb61120453cb2f1cbbf4b9a69e8cd62c9
Sejin Kim <kim3@kenyon.edu>
2020-03-23 11:24:43 -0400

    Update Ex3-1.py

--- Chapter3/Ex3-1.py
+++ Chapter3/Ex3-1.py
@@ -1,8 +1,8 @@
-from sklearn.datasets import fetch_openml
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 # Import the MNIST dataset
+from sklearn.datases import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
 mnist.keys()
 
@@ -27,3 +27,14 @@ y = y.astype(np.uint8)
 
 # Separate training and testing data
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# Training a binary classifier
+y_train_5 = (y_train == 5) # True for all 5s, false for all other numbers
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
